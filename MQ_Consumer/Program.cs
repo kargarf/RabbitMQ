8c3fbdd7069cea4eaf9b0d17a77f4eb13aeed683
ferruh KARGAR <kargar960@hotmail.com>
2022-12-22 14:50:27 +0300

    Add project files.

---
+++ MQ_Consumer/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://fjnesknx:g9B0IaNL03U57MLrHGlp3l0U-Boebcq9@hawk.rmq.cloudamqp.com/fjnesknx");//from rabbitmq cloud (https://www.cloudamqp.com/)
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.QueueDeclare("myqueue", true, false, false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume("myqueue", true, consumer);
+
+consumer.Received += Consumer_Received;
+
+void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+{
+    Console.WriteLine("Msj: " + Encoding.UTF8.GetString(e.Body.ToArray()));
+}
+
+Console.ReadLine();
---
+++ MQ_Publisherr/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://fjnesknx:g9B0IaNL03U57MLrHGlp3l0U-Boebcq9@hawk.rmq.cloudamqp.com/fjnesknx"); //from rabbitmq cloud
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.QueueDeclare("myqueue", true, false, false);
+
+const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+string strrandom = new string(Enumerable.Repeat(chars, 6).Select(s => s[Random.Shared.Next(s.Length)]).ToArray());
+
+var msg = strrandom + "_" + DateTime.Now.ToString("yyyyMMddTHH:mm:ss");
+var body = Encoding.UTF8.GetBytes(msg);
+
+channel.BasicPublish("", "myqueue", null, body);
+
+Console.WriteLine($"Message Published! {msg}");
+Console.ReadLine();
